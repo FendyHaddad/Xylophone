6408b8bdd719abd5654ae2db53e6b4de70aed28a
fendyhaddad <fendyhaddad@vankode.com>
2023-02-22 20:54:35 +0800

    Final Commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    AudioPlayer audioPlayer = AudioPlayer();
+
+    void playSound(int soundNumber) {
+      audioPlayer.play(AssetSource('note$soundNumber.wav'));
+    }
+
+    Expanded buildKey(backgroundColor, int soundNumber) {
+      return Expanded(
+        child: TextButton(
+          onPressed: () => playSound(soundNumber),
+          style: TextButton.styleFrom(backgroundColor: backgroundColor),
+          child: const Text(''),
+        ),
+      );
+    }
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Xylophone'),
+          backgroundColor: Colors.black,
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.blue, 4),
+              buildKey(Colors.green, 5),
+              buildKey(Colors.grey, 6),
+              buildKey(Colors.purple, 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
